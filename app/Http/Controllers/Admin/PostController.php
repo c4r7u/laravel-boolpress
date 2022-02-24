d956f345a0f3e780cb6869aed8ff82969c7e1f73
c4r7u <nicolo.carluccio3@gmail.com>
2022-02-24 11:44:33 +0100

    store done

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -31,7 +32,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -42,7 +43,17 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $form_data = $request->all();
+
+        $request->validate($this->getValidationRules());
+        $new_post = new Post();
+        $new_post->fill($form_data);
+        
+        $new_post->slug = $this->getUniqueSlugFromTitle($form_data['title']);
+
+        $new_post->save();
+
+        return redirect()->route('admin.posts.show', ['post' => $new_post->id]);      
     }
 
     /**
@@ -95,4 +106,31 @@ class PostController extends Controller
     {
         //
     }
+
+    protected function getValidationRules() {
+        return [
+            'title' => 'required|max:255',
+            'content' => 'required|max:60000'
+        ];
+    }
+
+    protected function getUniqueSlugFromTitle($title) {
+        // Controlliamo se esiste già un post con questo slug.
+        $slug = Str::slug($title);
+        $slug_base = $slug;
+        
+        $post_found = Post::where('slug', '=', $slug)->first();
+        $counter = 1;
+        while($post_found) {
+
+
+            $slug = $slug_base . '-' . $counter;
+            $post_found = Post::where('slug', '=', $slug)->first();
+            $counter++;
+        }
+
+        return $slug;
+    }
+
 }
+
